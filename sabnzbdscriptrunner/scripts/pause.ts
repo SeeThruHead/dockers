b4969b4918d0e372e735deb7e7e6630efe3f50b3
Shane Keulen <shane.keulen@influitive.com>
2021-12-26 14:12:27 -0500

    switch libraries

--- sabnzbdscriptrunner/scripts/pause.ts
+++ sabnzbdscriptrunner/scripts/pause.ts
@@ -1,5 +1,11 @@
-const foo: string = 'bob';
+import { SABnzbd } from "node-sabnzbd";
 
-console.log(process.env.FOO);
+const client = new SABnzbd("127.0.0.1", "8080", "6e961fd677a74326a07f1df4c06e3f38");
 
-console.log(foo);
+client.version().then((version: string) => {
+  console.log('success');
+  console.log(version);
+}).catch((error: { message: string }) => {
+  console.log('error');
+  console.log(error.message);
+});
